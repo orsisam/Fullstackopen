0df656c022893a53fab4770e267be30e87766dc5
Isro Hidayatulloh <orsi.amax@gmail.com>
2023-06-24 19:13:35 +0700

    exercise 1.3 step3

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -36,21 +36,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamental of React';
-  const exercise1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercise2 = 7;
-  const part3 = 'State of component';
-  const exercise3 = 14;
+  const part1 = {
+    name: 'Fundamental of React',
+    exercise: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercise: 7,
+  };
+  const part3 = {
+    name: 'State of component',
+    exercise: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part={[part1, part2, part3]}
-        exercise={[exercise1, exercise2, exercise3]}
+        part={[part1.name, part2.name, part3.name]}
+        exercise={[part1.exercise, part2.exercise, part3.exercise]}
       />
-      <Total total={exercise1 + exercise2 + exercise3} />
+      <Total total={part1.exercise + part2.exercise + part3.exercise} />
     </div>
   );
 };
