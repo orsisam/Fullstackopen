a9b9be07b37722c7e2e2acd1bc64cb32481fef44
Isro Hidayatulloh <orsi.amax@gmail.com>
2023-06-24 19:12:52 +0700

    part1.b JavaScript

---
+++ part1/JavaScript/arrays.js
@@ -0,0 +1,10 @@
+const t = [1, -1, 3]
+
+t.push(5)
+
+console.log(t.length)
+console.log(t[1])
+
+t.forEach(value => {
+    console.log(value)
+})
---
+++ part1/JavaScript/objects.js
@@ -0,0 +1,9 @@
+const object1 = {
+  name: 'Arto Helias',
+  age: 35,
+  education: 'PhD',
+};
+
+const object2 = {
+  name: 'Full Stack web application development',
+};
