84e386a9de2a3d2fb37bb330461fb867863cdfa5
Isro Hidayatulloh <orsi.amax@gmail.com>
2023-06-26 21:29:03 +0700

    Changes in state cause rerendering

--- part1/Practice/part1-c/src/App.js
+++ part1/Practice/part1-c/src/App.js
@@ -16,6 +16,7 @@ const Button = (props) => {
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log('rendering with counter value', counter);
 
   // Automatic increase counter
   // setTimeout(() => setCounter(counter + 1), 3000);
@@ -24,9 +25,18 @@ const App = () => {
   //   setCounter(counter + 1);
   // };
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
-  const decreaseByOne = () => setCounter(counter - 1);
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1);
+  };
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  };
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  };
 
   // to check the rendering process
   console.log('rendering...', counter);
