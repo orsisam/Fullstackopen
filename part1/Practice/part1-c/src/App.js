7299f17029a0d74c85079884e360a998b242214a
Isro Hidayatulloh <orsi.amax@gmail.com>
2023-06-26 20:50:02 +0700

    Event handling

--- part1/Practice/part1-c/src/App.js
+++ part1/Practice/part1-c/src/App.js
@@ -3,12 +3,29 @@ import { useState } from 'react';
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 3000);
+  // Automatic increase counter
+  // setTimeout(() => setCounter(counter + 1), 3000);
+
+  // const handleClick = () => {
+  //   setCounter(counter + 1);
+  // };
+
+  const increaseByOne = () => {
+    setCounter(counter + 1);
+  };
+
+  const setToZero = () => setCounter(0);
 
   // to check the rendering process
   console.log('rendering...', counter);
 
-  return <div>{counter}</div>;
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>reset</button>
+    </div>
+  );
 };
 
 export default App;
